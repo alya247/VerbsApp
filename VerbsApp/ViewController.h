b0a756eb7d6a242bc82d238e237908a1f2b68f4f
Admin <admin@mac-admin.local>
2016-08-07 15:50:41 +0300

    Initial Commit

---
+++ VerbsApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VerbsApp
+//
+//  Created by Admin on 07.08.16.
+//  Copyright © 2016 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
